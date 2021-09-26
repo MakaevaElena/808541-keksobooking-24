83e37804fc22c64349952bc77edce26dc51eb089
yamaslena@yandex.ru <yamaslena@yandex.ru>
2021-09-26 19:16:39 +0700

    исправлено:

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntInclusive(min, max) {
-  if (min >= max && min < 0) {
+  if (min >= max || min < 0) {
     return false;
   }
   min = Math.ceil(min); // округление вверх
   max = Math.floor(max); // округление вниз
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
-getRandomIntInclusive(); // Временный вызов функции
+getRandomIntInclusive(3,8); // Временный вызов функции
 
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
@@ -17,10 +17,10 @@ getRandomIntInclusive(); // Временный вызов функции
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 
 function getRandomArbitrary(min, max, count) {
-  if (min >= max && min < 0) {
+  if (min >= max || min < 0) {
     return false;
   }
   const RandomNum = Math.random() * (max - min) + min; // получаем случайное число в интервале.
   return RandomNum.toFixed(count); // toFixed() округляет дробную часть до count знаков после запятой.
 }
-getRandomArbitrary(); // Временный вызов функции
+getRandomArbitrary(3.45,96.567,2); // Временный вызов функции
