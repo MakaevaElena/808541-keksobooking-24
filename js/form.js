c1b06125befb3eeab37c801bb13466127ad17783
yamaslena@yandex.ru <yamaslena@yandex.ru>
2021-10-18 17:58:14 +0700

    убрана лишняя обработка филдсетов

--- js/form.js
+++ js/form.js
@@ -2,39 +2,21 @@ const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 
 const doFormDisable = (form) => {
-  form.classList.add(`${form.classList[0]}--disabled`); //дописывает к первому классу
+  form.classList.add(`${form.classList[0]}--disabled`);
 
-  for (const element of form.children) {
-    element.classList.add('disabled');
-  }
-
-  const fieldset = form.querySelectorAll('fieldset');
-  fieldset.forEach((element) => {
-    element.setAttribute('disabled', 'disabled');
-  });
-
-  const fieldsetChildren = Array.from(form.querySelectorAll('fieldset'));
-  fieldsetChildren.forEach((element) => {
+  const formChildren = Array.from(form.children);
+  formChildren.forEach((element) => {
     element.setAttribute('disabled', 'disabled');
   });
 };
 
-const doFormActive = (form) => {
+function doFormActive(form) {
   form.classList.remove(`${form.classList[0]}--disabled`);
 
-  for (const element of form.children) {
-    element.classList.remove('disabled');
-  }
-
-  const fieldset = form.querySelectorAll('fieldset');
-  fieldset.forEach((element) => {
+  const formChildren = Array.from(form.children);
+  formChildren.forEach((element) => {
     element.removeAttribute('disabled', 'disabled');
   });
-
-  const fieldsetChildren = Array.from(form.querySelectorAll('fieldset'));
-  fieldsetChildren.forEach((element) => {
-    element.removeAttribute('disabled', 'disabled');
-  });
-};
+}
 
 export { adForm, mapFilters, doFormDisable, doFormActive };
