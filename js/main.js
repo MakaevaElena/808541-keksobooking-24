2dd977502702195960d4f338d52d848253466959
yamaslena@yandex.ru <yamaslena@yandex.ru>
2021-10-25 10:45:46 +0700

    добавлены точки из externalData

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ import { externalData } from './data.js';
 import { popupAdsByTemp } from './getSameElements.js';
 import './getSameElements.js';
 import { adForm, mapFilters, doFormDisable, doFormActive } from './form.js';
-import { createMap } from './map.js';
+import { createMarker } from './map.js';
 
 popupAdsByTemp(externalData[1]);
 
 doFormDisable(adForm);
 doFormDisable(mapFilters);
 
-// createMap(externalData[1].location);
-createMap(externalData[1]);
+// createMap(externalData[1]);
+createMarker(externalData);
 
 doFormActive(adForm);
 doFormActive(mapFilters);
