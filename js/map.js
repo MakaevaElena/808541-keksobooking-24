396e6c6ae136024a303c40fdf3d91732c3fc1709
yamaslena@yandex.ru <yamaslena@yandex.ru>
2021-10-26 14:24:40 +0700

    координаты при загрузке карты

--- js/map.js
+++ js/map.js
@@ -1,31 +1,33 @@
-import { externalData } from './data.js';
 import { adForm } from './form.js';
 import { popupAdsByTemp } from './getSameElements.js';
 
-const MAIN_ICON_URL = 'img/main-pin.svg';
-const MAIN_ICON_SIZE = [52, 52];
-const MAIN_ICON_ANHOR = [26, 52];
-const MARKER_URL = 'img/pin.svg';
-const MARKER_SIZE = [40, 40];
-const MARKER_ANHOR = [20, 40];
-const resetButton = document.querySelector('#reset');
-const address = adForm.querySelector('#address');
-const MAIN_LAT = 35.65952;
-const MAIN_LNG = 139.78179;
+const DEFAULT_LAT_LNG = {
+  lat: 35.65952,
+  lng: 139.78179,
+};
 const RANGE_NUM = 5;
 const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const ZOOM = 16;
+const ZOOM = 10;
+const ICON = {
+  iconUrl: 'img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+};
+const MARKER = {
+  iconUrl: 'img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+};
+const address = adForm.querySelector('#address');
+const resetButton = document.querySelector('.ad-form__reset');
 
 const map = L.map('map-canvas')
-  .on('load', () => { // подсписка на события
+  .on('load', () => {
+    address.value = `${DEFAULT_LAT_LNG.lat},${DEFAULT_LAT_LNG.lng}`;
   })
-  .setView({ //параметры отображения карты — центр и масштаб.
-    lat: MAIN_LAT,
-    lng: 139.78179,
-  }, 10);
+  .setView(DEFAULT_LAT_LNG, 10);
 
-// изображения карт
 L.tileLayer(
   TILE_LAYER,
   {
@@ -33,56 +35,37 @@ L.tileLayer(
   },
 ).addTo(map);
 
-const mainPinIcon = L.icon({// добавили свою иконку svg
-  iconUrl: MAIN_ICON_URL,
-  iconSize: MAIN_ICON_SIZE,
-  iconAnchor: MAIN_ICON_ANHOR,
-});
+const mainPinIcon = L.icon(ICON);
 
-const mainPinMarker = L.marker( //добавление метки
-  {
-    lat: MAIN_LAT,
-    lng: MAIN_LNG,
-  },
+const mainPinMarker = L.marker(
+  DEFAULT_LAT_LNG,
   {
     draggable: true,
-    icon: mainPinIcon, // заменили на свою иконку
+    icon: mainPinIcon,
   },
 );
+mainPinMarker.addTo(map);
 
-mainPinMarker.addTo(map); //добавить метку на карту
-
-mainPinMarker.on('moveend', (evt) => {
+mainPinMarker.on('move', (evt) => {
   const { lat, lng } = evt.target.getLatLng();
   address.value = `${lat.toFixed(RANGE_NUM)}, ${lng.toFixed(RANGE_NUM)}`;
 });
 
-resetButton.addEventListener('click', () => { //вернуть иконку на исходное место
-  mainPinMarker.setLatLng({
-    lat: MAIN_LAT,
-    lng: MAIN_LNG,
-  });
-  map.setView({//вернуть исходный масштаб
-    lat: MAIN_LAT,
-    lng: MAIN_LNG,
-  }, ZOOM);
+resetButton.addEventListener('click', () => {
+  mainPinMarker.setLatLng(DEFAULT_LAT_LNG);
+  map.setView(DEFAULT_LAT_LNG, ZOOM);
+  address.value = `${DEFAULT_LAT_LNG.lat},${DEFAULT_LAT_LNG.lng}`;
 });
 
-// mainPinMarker.remove(); //удалить метку с карты
+const markerGroup = L.layerGroup().addTo(map);
 
-const markerGroup = L.layerGroup().addTo(map); //создание отдельного слоя для меток
-
-const createMarker = (points) => {//добавление своей иконки для меток
-  for (let i = 0; i < externalData.length; i++) {
+const createMarker = (points) => {
+  for (let i = 0; i < points.length; i++) {
     const lat = points[i].location.lat;
     const lng = points[i].location.lng;
-    const icon = L.icon({
-      iconUrl: MARKER_URL,
-      iconSize: MARKER_SIZE,
-      iconAnchor: MARKER_ANHOR,
-    });
+    const icon = L.icon(MARKER);
 
-    const marker = L.marker(//создание всех меток
+    const marker = L.marker(
       {
         lat,
         lng,
@@ -91,8 +74,8 @@ const createMarker = (points) => {//добавление своей иконки
         icon,
       },
     );
-    marker.addTo(markerGroup) //добавление всех меток на карту map / в слой markerGroup
-      .bindPopup(popupAdsByTemp(points[i]));//привяжем к каждой нашей метке балун
+    marker.addTo(markerGroup)
+      .bindPopup(popupAdsByTemp(points[i]));
   }
 };
 export { createMarker };
